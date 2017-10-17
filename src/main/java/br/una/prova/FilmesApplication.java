a8a43fe3c95581812231890c9932fefecafbafef
Guilherme Oliveira <gwoliveira@gmail.com>
2017-10-17 19:34:50 -0200

    filmes

--- src/main/java/br/una/prova/FilmesApplication.java
+++ src/main/java/br/una/prova/FilmesApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProvaApplication {
+public class FilmesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProvaApplication.class, args);
+		SpringApplication.run(FilmesApplication.class, args);
 	}
 }
